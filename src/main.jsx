645ea43ee93697e1eaf4f92484a96c70bbea403e
Phil Hawksworth <phil@netlify.com>
2024-05-10 18:20:44 +0100

    adding routes and views

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// src/main.jsx
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import DefaultLayout from './layouts/default-layout';
+import Index from './routes/index';
+import SignIn from './routes/sign-in';
+import SignUp from './routes/sign-up';
+import DashboardLayout from './layouts/dashboard-layout';
+import Dashboard from './routes/dashboard';
+const routes = createBrowserRouter([
+ {
+  element: <DefaultLayout />,
+  children: [
+   { path: '/', element: <Index /> },
+   { path: '/sign-in', element: <SignIn /> },
+   { path: '/sign-up', element: <SignUp /> },
+   {
+    element: <DashboardLayout />,
+    path: 'dashboard',
+    children: [{ path: '/dashboard', element: <Dashboard /> }],
+   },
+  ],
+ },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+<React.StrictMode>
+ <RouterProvider router={routes} />
+</React.StrictMode>
+);
